2e0cd5f6515cba0b778d8fa6e5331c5bfb4a9a0b
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-18 22:26:23 +0200

    Add timer lib

---
+++ timer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <chrono>
+#include <thread>
+
+class Timer {
+public:
+    Timer(void (*function)());
+    void setTimeout(std::chrono::milliseconds delay);
+    void setInterval(std::chrono::milliseconds interval);
+    void stop();
+private:
+    bool p_clear = false;
+    void (*function)();
+};
